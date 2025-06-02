d47183c95ca607fb5c370348976cb5b2b834bf49
Dzianis Shuranau <Dzianis_Shuranau@epam.com>
2025-06-02 17:55:46 +0300

    Added resources to the project

--- server.ts
+++ server.ts
@@ -1,10 +1,14 @@
-const express = require('express');
+import express from 'express';
+import path from 'path';
 
 const app = express();
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
+// Serve static files from public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
